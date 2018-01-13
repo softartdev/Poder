bdc1f0d5e52d4726ab9f87b703c41569b6861de2
artur <artik222012@gmail.com>
2018-01-14 00:29:42 +0500

    create main activity

--- app/src/main/java/com/softartdev/poder/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/softartdev/poder/ui/splash/SplashActivity.kt
@@ -1,14 +1,15 @@
 package com.softartdev.poder.ui.splash
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.softartdev.poder.ui.pokemon.PokemonActivity
+import android.support.v7.app.AppCompatActivity
+import com.softartdev.poder.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, PokemonActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
